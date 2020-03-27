04380822c72dc2c7e800c3784d5e2d46fc279f7f
Nery <nalbertoc132@gmail,com>
2020-03-27 01:16:07 -0400

    Divide the page into routes

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Badges from '../pages/Badges';
+import BadgesNew from '../pages/BadgeNew'
+
+function App(){
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/badges" component={Badges} />
+        <Route exact path="/badges/new" component={BadgesNew} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
